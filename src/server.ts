bfb2fbaebf3954d4cd738f1e4b2dad6f3a7edbc4
Patrick Wetherbee <pwetherb96@gmail.com>
2025-02-17 00:51:35 -0800

    fixed merge conflicts

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,26 @@
-import express from 'express';
-import chatRouter from './routes/chat.router';
+import express from "express";
+import { chatRouter } from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Server is running!');
+// create middleware to log requests that were recieved
+app.use(
+  (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log(`${req.method} ${req.path}`);
+    console.log(req.body);
+    next();
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
 });
 
-app.use('/v1/chat', chatRouter);
+app.use("/v1/chat", chatRouter);
 
-const port = process.env.PORT || 4444;
+const port = 4444;
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
