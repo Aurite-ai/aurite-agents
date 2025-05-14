d26ca8f92aedc9d1d5f95cb260cd27041be9aef0
Blake Randle <blake@aurite.ai>
2025-05-14 19:09:02 +0000

    clean up prompt validation

--- tests/prompt_validation/test_ab_testing.py
+++ tests/prompt_validation/test_ab_testing.py
@@ -25,9 +25,6 @@ class TestABTesting:
         else:
             pytest.skip("No json config specified. Use --config=[filename]")
 
-        # moved to host_manager initialize()
-        # await host_manager.register_config_file("config/prompt_validation_config.json")
-
         result = await host_manager.execution.run_custom_workflow(
             workflow_name="A/B Testing Workflow",
             initial_input={"config_path": testing_config_path},
