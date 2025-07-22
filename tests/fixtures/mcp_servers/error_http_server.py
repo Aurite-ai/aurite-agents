458edcab66742bd44797739889e676ea97495109
Blake Randle <blake@aurite.ai>
2025-07-22 19:21:39 +0000

    test tool timeout error

---
+++ tests/fixtures/mcp_servers/error_http_server.py
@@ -0,0 +1,14 @@
+import time
+
+from fastmcp import FastMCP
+
+mcp = FastMCP("Error Server")
+
+@mcp.tool
+def timeout(a: float, b: float) -> float:
+    # for testing timeout
+    time.sleep(10)
+    return a + b
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="127.0.0.1", port=8088, path="/mcp")
---
+++ tests/integration/mcp_servers/test_error.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+
+from aurite.config.config_models import ClientConfig
+from aurite.errors import MCPServerTimeoutError
+from aurite.host_manager import Aurite
+
+
+@pytest.mark.asyncio
+async def test_stdio_server_working(with_test_config):
+    """
+    Tests that a stdio server will handle a tool timeout properly
+    """
+    example_project_path = Path(".aurite").resolve()
+
+    async with Aurite(start_dir=example_project_path) as aurite:
+        host = aurite.kernel.execution._host
+
+        config_manager = aurite.get_config_manager()
+
+        config_manager.refresh()
+
+        await host.register_client(ClientConfig(**config_manager.get_config("mcp_server", "error_stdio_server")))
+
+        with pytest.raises(MCPServerTimeoutError) as e:
+            tool_result = await host.call_tool("error_stdio_server-timeout", {"a": 1, "b": 2})
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("start_http_server", ["tests/fixtures/mcp_servers/error_http_server.py"], indirect=True)
+async def test_http_server_error(with_test_config, start_http_server):
+    """
+    Tests that an http server will handle a tool timeout properly
+    """
+    example_project_path = Path(".aurite").resolve()
+
+    async with Aurite(start_dir=example_project_path) as aurite:
+        host = aurite.kernel.execution._host
+
+        config_manager = aurite.get_config_manager()
+
+        config_manager.refresh()
+
+        await host.register_client(ClientConfig(**config_manager.get_config("mcp_server", "error_http_server")))
+
+        with pytest.raises(MCPServerTimeoutError) as e:
+            tool_result = await host.call_tool("error_http_server-timeout", {"a": 1, "b": 2})
